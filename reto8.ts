4ef411b987fd5296291e007a703f21f511b6fe19
sbluff <alonsofmira@gmail.com>
2023-12-12 18:51:36 +0100

    code only inside function

--- reto8.ts
+++ reto8.ts
@@ -1,30 +1,31 @@
-interface GiftWrapping{
-  type: string,
-  count: number,
-}
-
-function RepeatString(str: string, num: number): string{
-  let result = ''
-  for (let i = 0; i < num; i++){
-    result += str 
-  }
-  return result
-}
-
-function Represent(gift: GiftWrapping): string{
-  const boxes = Math.floor(gift.count / 10) 
-  const bags = gift.count % 10
-
-  let boxes_representation = '', bag_representation = ''
-  let pales = Math.floor(boxes / 5)
-  boxes_representation += (pales != 0) ? '[' + RepeatString(gift['type'], pales) + ']' : ''
-  boxes_representation += (boxes%5 != 0) ? RepeatString('{' + gift['type']+ '}', boxes % 5) : ''
-  bag_representation += '(' + RepeatString(gift['type'], bags) + ')'
-  
-  return boxes_representation + bag_representation
-}
-
 function organizeGifts(gifts): string {
+  interface GiftWrapping{
+    type: string,
+    count: number,
+  }
+
+  const Represent = (gift: GiftWrapping):string => {
+    const boxes = Math.floor(gift.count / 10) 
+    const bags = gift.count % 10
+
+    let boxes_representation = '', bag_representation = ''
+    let pales = Math.floor(boxes / 5)
+    boxes_representation += (pales != 0) ? '[' + RepeatString(gift['type'], pales) + ']' : ''
+    boxes_representation += (boxes%5 != 0) ? RepeatString('{' + gift['type']+ '}', boxes % 5) : ''
+    bag_representation += '(' + RepeatString(gift['type'], bags) + ')'
+    
+    return boxes_representation + bag_representation
+  }
+
+  const RepeatString = (str: string, num: number): string => {
+    let result = ''
+    for (let i = 0; i < num; i++){
+      result += str 
+    }
+    return result
+  }
+
+
   const organized_gifts: {[key: string]: GiftWrapping} = {}
   let start_index = 0                                       //to get the substrings 
   for (let i = 0; i < gifts.length; i++){
