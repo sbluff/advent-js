a155dcac618b5d0ed8411cb08e1ddc4e8be92186
sbluff <alonsofmira@gmail.com>
2023-12-14 11:27:27 +0100

    upgrade challenge 9 (330 points)

--- reto9.ts
+++ reto9.ts
@@ -2,26 +2,26 @@ function adjustLights(lights){
   if (lights.length == 0)
     return 0
 
-  let expected_color = lights[0], counter = 0;
-  for (let i = 0; i < lights.length; i++){
-    if (lights[i] !== expected_color){
-      counter += 1
-    }
-
-    expected_color = (expected_color === '🟢') ? '🔴' : '🟢' 
-  } 
-
-  let counter_reverse = 0 
-  expected_color = (lights[0] === '🟢') ? '🔴' : '🟢' 
-  for (let i = 0; i < lights.length; i++){
-    if (lights[i] !== expected_color){
-      counter_reverse += 1
-    }
-
-    expected_color = (expected_color === '🟢') ? '🔴' : '🟢' 
+  const counter = {
+    forward: 0,
+    backward: 0,
   }
 
-  return Math.min(counter, counter_reverse)
+  for (const mode in counter){
+    let reverse_color = lights[0] === '🟢' ? '🔴' : '🟢' 
+    let expected_color = (mode === 'forward') ? lights[0] : reverse_color;
+
+    for (let i = 0; i < lights.length; i++){
+      if (lights[i] !== expected_color){
+        counter[mode] += 1
+      }
+  
+      expected_color = (expected_color === '🟢') ? '🔴' : '🟢' 
+    } 
+  }
+
+  return Math.min(counter.forward, counter.backward)
 }
 
-adjustLights(['🔴', '🔴', '🔴'])
+let a = adjustLights(['🟢', '🔴', '🟢', '🟢', '🟢'])
+console.log(a)
